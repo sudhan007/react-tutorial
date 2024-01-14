a0a67166b624d4c433a144fbf065021e1c8cb9c8
Sudhan007 <sudhancool87@gmail.com>
2024-01-14 19:38:29 +0530

    first_l

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import app from "./src/app.js";
-
-app.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-  app.log.info(`server listening on ${address}`);
-});
--- api/src/middlewares/jwttoken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { envConfig } from '../config/env.js';
-
-const Jwttoken = async (data) => {
-  const token = jwt.sign(data, envConfig.jwtKey);
-  return token;
-};
-
-export default Jwttoken;
